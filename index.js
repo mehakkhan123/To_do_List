844322eceac35acb36d85863b3b14c54963e6b52
mehakkhan123 <mahekkhan284@gmail.com>
2024-03-31 12:17:45 +0500

    to do list

---
+++ index.js
@@ -0,0 +1,22 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let toDoList = [];
+let condition = true;
+while (condition) {
+    let input = await inquirer.prompt([
+        {
+            name: "add",
+            type: "input",
+            message: "What do you want to add in your todo list?",
+        },
+        {
+            name: "addMore",
+            type: "confirm",
+            message: "Do you want to add more in your todo list?",
+            default: "false",
+        }
+    ]);
+    toDoList.push(input.add);
+    condition = input.addMore;
+    console.log(toDoList);
+}
